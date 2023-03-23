491a13d3cbaf825fed87501dc83f54b85cc418d5
1xtend <vlad.mezhevyi@gmail.com>
2023-03-23 15:23:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const body = document.body;
+
+burger.addEventListener('click', (e) => {
+  burger.classList.toggle('is-active');
+  navbar.classList.toggle('is-active');
+  body.classList.toggle('is-fixed');
+
+  navbar.addEventListener('click', (e) => {
+    if (e.target !== e.currentTarget) {
+      burger.classList.remove('is-active');
+      navbar.classList.remove('is-active');
+      body.classList.remove('is-fixed');
+    }
+  });
+});
